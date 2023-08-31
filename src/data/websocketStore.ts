3a829a31f5df0959ecdb569c8b01d0e49b002235
Barber <barber@MT-306070.jpl.nasa.gov>
2023-08-31 12:45:33 +0100

    update websocket location

--- src/data/websocketStore.ts
+++ src/data/websocketStore.ts
@@ -1,11 +1,12 @@
-import { createWebsocketStore } from "../store/websocketstore";
-import type {WebsocketStore} from "../store/websocketstore"
+import { createWebsocketStore } from '../store/websocketstore';
+import type { WebsocketStore } from '../store/websocketstore';
 
 // ... (rest of your existing code)
 
 // Define the WebSocket URL
 //const url: string = 'ws://' + window.location.host + '/ws';
-const url = 'ws://localhost:8080/ws'
+const url: string =
+	typeof window !== 'undefined' ? `ws://${window.location.host}/ws` : 'ws://localhost/ws';
 
 // Create the WebSocket store using the provided function
 export const websocketStore: WebsocketStore = createWebsocketStore(url);
--- src/store/websocketstore.ts
+++ src/store/websocketstore.ts
@@ -1,83 +1,83 @@
-import { writable, derived } from "svelte/store";
-import type { Writable } from "svelte/store";
+import { writable, derived } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 import type { Readable } from 'svelte/store';
-import  {  UnityWebsocketMessage } from "../data/unity-cs-manager/protobuf/extensions";
+import { UnityWebsocketMessage } from '../data/unity-cs-manager/protobuf/extensions';
 
 type WritableStore = Writable<UnityWebsocketMessage[]>;
 
 export type WebsocketStore = {
-  subscribe: WritableStore['subscribe'];
-  send: (message: Uint8Array) => void;
-  filterByType: (type: keyof UnityWebsocketMessage) => Readable<UnityWebsocketMessage[]>;
+	subscribe: WritableStore['subscribe'];
+	send: (message: Uint8Array) => void;
+	filterByType: (type: keyof UnityWebsocketMessage) => Readable<UnityWebsocketMessage[]>;
 };
-function createWebsocketStore(url: string) : WebsocketStore  {
-  const { subscribe, set, update } = writable<UnityWebsocketMessage[]>([]);
 
-  let socket: WebSocket;
-  let messageQueue: Uint8Array[] = [];
+function createWebsocketStore(url: string): WebsocketStore {
+	const { subscribe, update } = writable<UnityWebsocketMessage[]>([]);
 
-  function connect() {
-    if (typeof WebSocket === 'undefined') {
-      console.error('WebSocket is not available in this environment');
-      return;
-    }
-    socket = new WebSocket(url);
+	let socket: WebSocket;
+	let messageQueue: Uint8Array[] = [];
 
-    socket.onopen = () => {
-      console.log('Socket is open');
-      sendQueuedMessages();
-    };
+	function connect() {
+		if (typeof WebSocket === 'undefined') {
+			console.error('WebSocket is not available in this environment');
+			return;
+		}
+		socket = new WebSocket(url);
 
-    socket.onmessage = async (event) => {
-      const response = new Response(event.data);
-      const arrayBuffer = await response.arrayBuffer();
-      const data = new Uint8Array(arrayBuffer);
-      const message = UnityWebsocketMessage.decode(data); // Decode the Protobuf message
-      update(messages => [...messages, message]);
-    };
+		socket.onopen = () => {
+			console.log('Socket is open');
+			sendQueuedMessages();
+		};
 
-    socket.onerror = (error) => {
-      console.error('WebSocket error: ', error);
-    };
+		socket.onmessage = async (event) => {
+			const response = new Response(event.data);
+			const arrayBuffer = await response.arrayBuffer();
+			const data = new Uint8Array(arrayBuffer);
+			const message = UnityWebsocketMessage.decode(data); // Decode the Protobuf message
+			update((messages) => [...messages, message]);
+		};
 
-    socket.onclose = (event) => {
-      console.log('Socket is closed. Reconnect will be attempted in 1 second.', event.reason);
-      setTimeout(function() {
-        connect();
-      }, 1000);
-    };
-  }
+		socket.onerror = (error) => {
+			console.error('WebSocket error: ', error);
+		};
 
-  connect();
-  function sendQueuedMessages() {
-    messageQueue.forEach((message) => {
-      send(message);
-    });
-    messageQueue = [];
-  }
-  function send(message: Uint8Array) {
-    if (socket.readyState === WebSocket.OPEN) {
-      socket.send(message);
-    } else {
-      console.log('Socket is not open. Queueing the message.');
-      messageQueue.push(message);
-    }
-  }
+		socket.onclose = (event) => {
+			console.log('Socket is closed. Reconnect will be attempted in 1 second.', event.reason);
+			setTimeout(function () {
+				connect();
+			}, 1000);
+		};
+	}
 
-  function filterByType(type: keyof UnityWebsocketMessage): Readable<UnityWebsocketMessage[]> {
-    return derived(
-      { subscribe },
-      ($messages, set) => {
-        set($messages.filter((message: UnityWebsocketMessage) => message[type] !== undefined));
-      }
-    );
-  }
+	connect();
 
-  return {
-    subscribe,
-    send,
-    filterByType,
-  };
+	function sendQueuedMessages() {
+		messageQueue.forEach((message) => {
+			send(message);
+		});
+		messageQueue = [];
+	}
+
+	function send(message: Uint8Array) {
+		if (socket.readyState === WebSocket.OPEN) {
+			socket.send(message);
+		} else {
+			console.log('Socket is not open. Queueing the message.');
+			messageQueue.push(message);
+		}
+	}
+
+	function filterByType(type: keyof UnityWebsocketMessage): Readable<UnityWebsocketMessage[]> {
+		return derived({ subscribe }, ($messages, set) => {
+			set($messages.filter((message: UnityWebsocketMessage) => message[type] !== undefined));
+		});
+	}
+
+	return {
+		subscribe,
+		send,
+		filterByType
+	};
 }
 
 export { createWebsocketStore };
