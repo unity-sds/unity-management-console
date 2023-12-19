53113a01abe03c7c793e51bb5dbd50e2b453f812
Barber <barber@MT-306070.jpl.nasa.gov>
2023-12-19 10:41:46 +0000

    update ws target

--- src/data/websocketStore.ts
+++ src/data/websocketStore.ts
@@ -3,7 +3,9 @@ import type { WebsocketStore } from '../store/websocketstore';
 
 // Define the WebSocket URL
 const url: string =
-	typeof window !== 'undefined' ? `ws://${window.location.host}/ws` : 'ws://localhost:8080/ws';
+	typeof window !== 'undefined'
+		? `ws://${window.location.host}/management/ws`
+		: 'ws://localhost:8080/ws';
 
 // Create the WebSocket store using the provided function
 export const websocketStore: WebsocketStore = createWebsocketStore(url);
