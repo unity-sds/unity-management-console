643017a8da02b4ce45d3de5b8980411a57877868
barber <tom@spicule.co.uk>
2024-02-28 09:07:20 +0000

    update backend lookup for venue and project

--- backend/internal/processes/bootstrap.go
+++ backend/internal/processes/bootstrap.go
@@ -175,7 +175,7 @@ func installGateway(store database.Datastore, appConfig *config.AppConfig) error
 	variables := marketplace.Install_Variables{Values: simplevars}
 	applications := marketplace.Install_Applications{
 		Name:        "unity-proxy",
-		Version:     "0.7",
+		Version:     "0.8",
 		Variables:   &variables,
 		Displayname: fmt.Sprintf("%s-%s", appConfig.InstallPrefix, "unity-proxy"),
 	}
