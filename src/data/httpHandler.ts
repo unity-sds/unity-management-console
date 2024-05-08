85aeec65b0b86633f9957087882bbab82f27dd97
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-05-08 15:33:17 -0700

    update

--- src/data/httpHandler.ts
+++ src/data/httpHandler.ts
@@ -27,7 +27,6 @@ let marketplaceowner = 'unity-sds';
 let marketplacerepo = 'unity-marketplace';
 
 const unsubscribe = config.subscribe((configValue) => {
-	console.log(configValue);
 	if (configValue && configValue.applicationConfig && configValue.applicationConfig.GithubToken) {
 		headers = {
 			Authorization: `token ${configValue.applicationConfig.GithubToken}`
