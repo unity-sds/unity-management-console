b9624348aaffdfb3f22b5ec5d47cb0aff97f753f
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-05-20 19:36:43 -0700

    wip

--- backend/internal/web/api.go
+++ backend/internal/web/api.go
@@ -54,6 +54,7 @@ func handleAPICall(appConfig config.AppConfig) gin.HandlerFunc {
 		// Read the object and pass the data on to the requester
 		log.Warnf("%v", *latestHealthCheckObject.Key)
 		object := aws.GetObject(nil, &appConfig, *bucketNameParam.Parameter.Value, *latestHealthCheckObject.Key)
+		log.Warnf("%d", len(object))
 		c.Data(http.StatusOK, "application/json", object)
 		
 		return
