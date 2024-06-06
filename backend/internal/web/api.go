f5a5eaba1cfb7fd5490c66d79812c3df7163095d
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-06-06 16:13:58 -0700

    wip

--- backend/internal/web/api.go
+++ backend/internal/web/api.go
@@ -1,61 +1,53 @@
 package web
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/gin-gonic/gin"
-	// log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/unity-sds/unity-management-console/backend/internal/application/config"
 	"github.com/spf13/viper"
-	// "github.com/unity-sds/unity-management-console/backend/internal/aws"
+	"github.com/unity-sds/unity-management-console/backend/internal/aws"
 	// "github.com/unity-sds/unity-management-console/backend/internal/database"
-	// "github.com/aws/aws-sdk-go-v2/service/s3/types"
-	// strftime "github.com/ncruces/go-strftime"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	strftime "github.com/ncruces/go-strftime"
 	"net/http"
-	// "time"
+	"time"
 )
 
 func handleAPICall(appConfig config.AppConfig) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		// Get the location of the health check bucket
-		// healthCheckParamPath := fmt.Sprintf("/unity/deployment/%s/%s/cs/monitoring/s3/bucketName", appConfig.Project, appConfig.Venue)
-		// bucketNameParam, err := aws.ReadSSMParameter(healthCheckParamPath)
+		bucketname := viper.Get("bucketname")
 
-		// if err != nil {
-		// 	log.Warnf("Health check bucket name: %s", *bucketNameParam.Parameter.Value)
-		// }
+		// Get a listing of all the files in the bucket and pick the one with the latest timestamp
+		result := aws.ListObjectsV2(nil, &appConfig, bucketname, "health_check")
 
-		// log.Warnf("Bucket Location: %s", bucketNameParam)
+		layout, err := strftime.Layout("health_check_%Y-%m-%d_%H-%M-%S.json")
+		if err != nil {
+			log.Warnf("%s", "Error parsing date layout")
+		}
 
-		// // Get a listing of all the files in the bucket and pick the one with the latest timestamp
-		// result := aws.ListObjectsV2(nil, &appConfig, *bucketNameParam.Parameter.Value, "health_check")
+		var latestHealthCheckObject *types.Object
+		var latestHealthCheckDatetime *time.Time
 
-		// layout, err := strftime.Layout("health_check_%Y-%m-%d_%H-%M-%S.json")
-		// if err != nil {
-		// 	log.Warnf("%s", "Error parsing date layout")
-		// }
+		for i, object := range result {
+			t, err := time.Parse(layout, *object.Key)
 
-		// var latestHealthCheckObject *types.Object
-		// var latestHealthCheckDatetime *time.Time
+			if err != nil || t.IsZero() {
+				log.Warnf("File Doesn't Match: %s", *object.Key)
+				continue
+			}
 
-		// for i, object := range result {
-		// 	t, err := time.Parse(layout, *object.Key)
+			if latestHealthCheckObject == nil || t.After(*latestHealthCheckDatetime) {
+				latestHealthCheckObject = &result[i]
+				latestHealthCheckDatetime = &t
+			}
+		}
 
-		// 	if err != nil || t.IsZero() {
-		// 		log.Warnf("File Doesn't Match: %s", *object.Key)
-		// 		continue
-		// 	}
-
-		// 	if latestHealthCheckObject == nil || t.After(*latestHealthCheckDatetime) {
-		// 		latestHealthCheckObject = &result[i]
-		// 		latestHealthCheckDatetime = &t
-		// 	}
-		// }
-
-		// if latestHealthCheckObject == nil {
-			jsonData := []byte(fmt.Sprintf(`{"error": "%s"}`,viper.Get("bucketname")))
-			// jsonData := []byte(`{"error": "Can't find any health check files"}`)
+		if latestHealthCheckObject == nil {
+			// jsonData := []byte(fmt.Sprintf(`{"error": "%s"}`,viper.Get("bucketname")))
+			jsonData := []byte(`{"error": "Can't find any health check files"}`)
 			c.Data(http.StatusOK, "application/json", jsonData)
-		// }
+		}
 
 		// // Read the object and pass the data on to the requester
 		// object := aws.GetObject(nil, &appConfig, *bucketNameParam.Parameter.Value, *latestHealthCheckObject.Key)
