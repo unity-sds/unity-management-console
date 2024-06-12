e97490b10264133653e06d4f0968ca308ffb3a79
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-06-11 22:14:16 -0700

    wip

--- backend/internal/processes/bootstrap.go
+++ backend/internal/processes/bootstrap.go
@@ -197,7 +197,7 @@ func installBasicAPIGateway(store database.Datastore, appConfig *config.AppConfi
 	variables := marketplace.Install_Variables{Values: simplevars}
 	applications := marketplace.Install_Applications{
 		Name:        "unity-apigateway",
-		Version:     "0.3",
+		Version:     "0.4",
 		Variables:   &variables,
 		Displayname: fmt.Sprintf("%s-%s", appConfig.InstallPrefix, "unity-apigateway"),
 	}
