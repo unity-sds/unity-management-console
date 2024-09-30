d5312a61b78ca1c9e34dac72fc50cb8cb799d1f8
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-09-30 09:21:54 -0700

    wip

--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -18,15 +18,26 @@
   $: console.log(product);
 </script>
 
-<div class="container">
-  <div class="row">
+<div class="container" class="container">
+  <div class="st-typography-displayH3">
+    Installing Marketplace Application: {product.DisplayName}
+  </div>
+  <!-- <div class="row">
     <div class="col-md-12">
       {#if product}
+
         <h1 class="my-4">{product.DisplayName} Installation</h1>
         <SetupWizard />
       {:else}
         <p>Loading product...</p>
       {/if}
-    </div>
-  </div>
+    </div> -->
+  <!-- </div> -->
 </div>
+
+<style>
+  .container {
+    display: flex;
+    margin:10px;
+  }
+</style>
