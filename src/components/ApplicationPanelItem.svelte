1d8dec2f1b636e5c99ab804a9b0588d1cc5f232d
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-10-01 09:57:34 -0700

    wip

--- src/components/ApplicationPanelItem.svelte
+++ src/components/ApplicationPanelItem.svelte
@@ -81,8 +81,15 @@
 
   let showLogs = false;
   let logInterval: any = null;
+  let logs = '';
   async function getLogs() {
     const res = await fetch(`../api/uninstall_application/status/${deployment}`);
+    if (!res.ok) {
+      console.warn("Can't get logs!");
+      clearInterval(logInterval);
+      return;
+    }
+    logs = await res.text();
   }
   $: if (showLogs && !logInterval) {
     logInterval = setInterval((_) => {
@@ -141,32 +148,21 @@
         class="st-button secondary"
         style="margin-top: 5px;"
         on:click={(_) => (showLogs = !showLogs)}
-        >Uninstall Error
+        >Show Logs
       </button>
     </div>
   </div>
 </div>
 
 <Modal bind:showModal={showLogs}>
-  <h2 slot="header">
+  <!-- <h2 slot="header">
     modal
     <small><em>adjective</em> mod·al \ˈmō-dəl\</small>
   </h2>
-
-  <ol class="definition-list">
-    <li>of or relating to modality in logic</li>
-    <li>
-      containing provisions as to the mode of procedure or the manner of taking effect —used of a
-      contract or legacy
-    </li>
-    <li>of or relating to a musical mode</li>
-    <li>of or relating to structure as opposed to substance</li>
-    <li>
-      of, relating to, or constituting a grammatical form or category characteristically indicating
-      predication
-    </li>
-    <li>of or relating to a statistical mode</li>
-  </ol>
-
-  <a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+ -->
+  {#if logs}
+    <pre>
+  {logs}
+</pre>
+  {/if}
 </Modal>
