ac0bfd688f7b270f9c1dd762b8b7f9e67ea08899
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2024-10-10 15:22:02 -0700

    wip

--- src/components/ApplicationPanelItem.svelte
+++ src/components/ApplicationPanelItem.svelte
@@ -148,7 +148,7 @@
       <span class="st-typography-header">{title}</span>
       <span class="st-typography-bold">Application: 1{appName}</span>
       <div style="display: flex; gap: 10px; margin: 10px; justify-content: center">
-        <image height="32" width="32" src={CloseIcon} />
+        <img height="32" width="32" src={CloseIcon} />
         <span class="st-typography-bold">Installation Status:</span>
         {#if combinedStatus === 'COMPLETE'}
           <span class="st-typography-small-caps" style="color: green">Done</span>
