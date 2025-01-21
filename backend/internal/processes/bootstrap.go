aba08a20a62f94acd0358aff126577ff952e34e7
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2025-01-21 09:42:33 -0800

    wip

--- backend/internal/processes/bootstrap.go
+++ backend/internal/processes/bootstrap.go
@@ -137,6 +137,12 @@ func BootstrapEnv(appconf *config.AppConfig) error {
 		return err
 	}
 
+	log.Infof("Setting Up Core Config SSM Params")
+	err = UpdateCoreConfig(appconf, store, nil, "")
+	if err != nil {
+		log.WithError(err).Error("Problem updating ssm config")
+	}
+
 	log.Infof("Bootstrap Process Completed Succesfully")
 	return nil
 }
