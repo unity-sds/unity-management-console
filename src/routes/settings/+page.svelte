f41376d1d3dc544fb5f4bfde274afa6dd0085b18
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2025-02-03 18:29:48 -0800

    wip

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,1 +1,35 @@
-<p>Here's where we can see some settings!</p>
+<script lang="ts">
+	// import { TreeView } from 'carbon-components-svelte';
+
+	interface Parameter {
+		key: string;
+		value: string;
+	}
+
+	interface ParameterResponse {
+		parameterlist: Record<string, Parameter>;
+	}
+
+	async function getSSMParams(): Promise<ParameterResponse> {
+		const res = await fetch('../api/ssm_params/current', { method: 'GET' });
+		if (res.ok) {
+			const res = await res.json();
+			const nodes = Object.entries(res).reduce((acc, [key, param]) => {
+				const base = key.split('/')[0];
+				acc.push(base);
+				return acc;
+			}, []);
+			console.log(nodes);
+		}
+		return <ParameterResponse>{};
+	}
+</script>
+
+<div class="container mx-auto px-4">
+	<h4>SSM Params Navigator</h4>
+	{#await getSSMParams()}
+		<strong>Loading</strong>
+	{:then res}
+		Done!
+	{/await}
+</div>
