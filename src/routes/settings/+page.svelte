ee9e25e7310fd6d6fe471596f598ac7565ed907e
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2025-02-03 18:47:45 -0800

    wip

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -15,20 +15,24 @@
 		nodes: ParamNode[];
 	}
 
+	function parseNodes(rootNode) {
+		return Object.entries(rootNode).reduce((acc, [key, param]) => {
+			const base = key.split('/')[1];
+			const prefixIndex = acc.findIndex((node) => node.text === base);
+			if (prefixIndex >= 0) {
+				acc[prefixIndex].nodes.push({ text: key, nodes: [] });
+			} else {
+				acc.push({ text: base, nodes: [{ text: key, nodes: [] }] });
+			}
+			return acc;
+		}, [] as ParamNode[]);
+	}
+
 	async function getSSMParams(): Promise<ParameterResponse> {
 		const res = await fetch('../api/ssm_params/current', { method: 'GET' });
 		if (res.ok) {
 			const json = await res.json();
-			const nodes = Object.entries(json.parameterlist).reduce((acc, [key, param]) => {
-				const base = key.split('/')[1];
-				const prefixIndex = acc.findIndex((node) => node.text === base);
-				if (prefixIndex >= 0) {
-					acc[prefixIndex].nodes.push({ text: key, nodes: [] });
-				} else {
-					acc.push({ text: base, nodes: [{ text: key, nodes: [] }] });
-				}
-				return acc;
-			}, [] as ParamNode[]);
+			const nodes = parseNodes(json.parameterlist);
 			console.log(nodes);
 		}
 		return <ParameterResponse>{};
